1270c3fa32f905d4b2719fcae2bdcb1f5bd8df1f
Eszti2222 <nagy.eszter2222@gmail.com>
2025-03-27 10:20:04 +0100

    konst, set+get

---
+++ Lampa.js
@@ -0,0 +1,37 @@
+export default class Lampa {
+  #szin;
+  #index;
+  #szElem;
+
+  constructor(szin, index, szElem) {
+    this.#szin = szin;
+    this.#index = index;
+    this.#szElem = szElem;
+    this.getSzin();
+    this.setMellette();
+
+    this.#szElem.addEventListener("click", () => {
+      if (this.#szin === this.#index.#szin) {
+        const e = new CustomEvent("kivalaszt", { detail: this.#index });
+        window.dispatchEvent(e);
+      }
+    });
+  }
+  megjelenit() {
+    let html = `<div class="lampa">
+                        <p>${this.#szin}</p>
+                    </div>
+                    `;
+    this.szElem.insertAdjacentHTML("beforeend", html);
+  }
+
+  getSzin() {
+    return this.#szin;
+  }
+  setMellette(){
+    aktSzin=this.getSzin()
+    if(kattintas){
+        this.#index+= !aktSzin;
+    }
+  }
+}
